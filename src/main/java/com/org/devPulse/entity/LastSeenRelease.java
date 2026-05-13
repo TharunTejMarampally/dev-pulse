0abadc6296d4b6a2571f91a752481f4e6a35b126
Tharun <tharunmarampally@gmail.com>
2026-05-14 00:25:14 +0530

    - import changed for ID

--- src/main/java/com/org/devPulse/entity/LastSeenRelease.java
+++ src/main/java/com/org/devPulse/entity/LastSeenRelease.java
@@ -1,11 +1,11 @@
 package com.org.devPulse.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 
 @Entity
 @Getter
